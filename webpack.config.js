0663a60fad604aaea78df38b91c1319734e08b23
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-11-15 14:44:47 -0500

    Init from other project.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var webpack = require('webpack');
+//var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./client/app.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: '[name].js'
+  },
+
+  //plugins: [
+  //  new webpack.DefinePlugin({
+  //    'process.env.NODE_ENV': JSON.stringify('dev')
+  //  })
+  //],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: /client/,
+        //exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.md$/,
+        include: /page/,
+        //exclude: /node_modules/,
+        loader: 'markdown_loader'
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+    ]
+
+  }
+
+};
