1942b8cbf06cf9ebe12c8efbf117fe22ae15b9f8
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-11-28 14:32:02 -0500

    Canvas test page.

--- client/control/home_model.js
+++ client/control/home_model.js
@@ -1,27 +1,19 @@
 //import {div, span, p, a, ul, li, br, h1, h2, h3, input, select, option} from 'specdom_helper';
-var div, span, p, a, ul, li, br, h1, h2, h3, input, select, option;
+//var div, span, p, a, ul, li, br, h1, h2, h3, input, select, option;
 var specdom_helper = require('specdom_helper');
-
-console.log(specdom_helper, typeof specdom_helper);
+import $ from 'simpledom';
+console.log('$', $);
+//console.log(specdom_helper, typeof specdom_helper);
 
 export default function(){
 
-
-  target_element.append($({
-    tag: 'canvas',
-    props: {
-      id: 'canvas',
-      width: 1500,
-      height: 1500
-    }
-  }));
-  var canvas = document.getElementById('canvas');
-  canvas.style.backgroundColor = 'white';
+  var canvas = $('#canvas');
+  console.log('canvas' ,canvas);
+  canvas.elem.style.backgroundColor = 'green';
 
 
 
 
 
 
-  return spec;
 }
