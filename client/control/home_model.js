4170a7fedb3c41f6127640f86d36fa5b82c2edea
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-12-07 16:39:30 -0500

    Added group shape.

--- client/control/home_model.js
+++ client/control/home_model.js
@@ -1,5 +1,5 @@
 import draw_on_canvas from '../../lib/draw_on_canvas';
-import mk_shape from '../../lib/shape_maker';
+import mk_shape from '../../lib/mk_shape';
 import mouse from '../../lib/mouse';
 import shapes from './home_model/shapes';
 import click from '../../lib/click';
@@ -9,6 +9,7 @@ var global = window || global;
 
 var state = global.page_state = {
   specs: {},
+  update: true,
   shapes: {},
   locations: {
     center: {
@@ -20,28 +21,34 @@ var state = global.page_state = {
 };
 state.shapes = shapes(state);
 
+
 var id = 'canvas';
 var canvas;
 //console.log('canvas', canvas);selected
 
 function draw() {
-  var ctx = document.getElementById('canvas').getContext('2d');
+  if( state.update ){
+    console.log('|>raw');
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
 
-  state.specs = [
-    mk_shape('fill', 'white', 'rect', [0, 0, canvas.width, canvas.height]),
-  ];
+    state.specs = [
+      mk_shape('rect', 'fill', 'white', [0, 0, canvas.width, canvas.height]),
+    ];
 
-  state.shapes.forEach(function(shape){
-    state.specs.push(shape);
-  });
+    state.shapes.forEach(function(shape){
+      state.specs.push(shape);
+    });
 
-  ctx.save();
+    ctx.save();
 
-  draw_on_canvas(canvas, state.specs);
+    draw_on_canvas(canvas, state.specs);
 
-  ctx.restore();
+    ctx.restore();
 
-  window.requestAnimationFrame(draw);
+    state.update = false;
+    window.requestAnimationFrame(draw);
+  }
 }
 
 var actions = {
--- client/control/home_model/shapes.js
+++ client/control/home_model/shapes.js
@@ -1,40 +1,50 @@
-import mk_shape from '../../../lib/shape_maker';
+import mk_shape from '../../../lib/mk_shape';
 import link_shapes from '../../../lib/link_shapes';
 
 export default function(state){
+  var shape;
+  var group;
   var shapes = [];
 
-  var x = state.locations.center.x;
-  var y = state.locations.center.y;
-  var w;
-  var h;
+  var x, y, w, h;
 
+  x = state.locations.center.x;
+  y = state.locations.center.y;
+
+  group = mk_shape('group');
+  shapes.push(group);
 
   w = 100;
   h = 300;
-  shapes.push(mk_shape('stroke', 'red', 'rect', [x-w/2,y-h/2,w,h]));
+  shape = mk_shape('rect', 'stroke', 'red', [x-w/2,y-h/2,w,h], [
+    mk_shape('rect', 'stroke', 'blue', [x-w/2,y-h/2,w,h])
+  ]);
+  group.children.push(shape);
 
   w = 300;
   h = 100;
-  shapes.push(mk_shape('fill', 'blue', 'rect', [x-w/2,y-h/2,w,h]));
-
+  shape = mk_shape('rect', 'fill', 'red', [x-w/2,y-h/2,w,h], [
+    mk_shape('rect', 'fill', 'blue', [x-w/2,y-h/2,w,h])
+  ]);
+  group.children.push(shape);
 
   x = 100;
   y = 100;
   w = 100;
   h = 100;
-  shapes.push(mk_shape('stroke', 'blue', 'rect', [x-w/2,y-h/2,w,h], [
-    mk_shape('fill', 'blue', 'rect', [x-w/2,y-h/2,w,h]).props
-  ]));
+  shape = mk_shape('rect', 'stroke', 'blue', [x-w/2,y-h/2,w,h], [
+    mk_shape('rect', 'fill', 'blue', [x-w/2,y-h/2,w,h])
+  ]);
+  shapes.push(shape);
 
-
-  x = 500;
-  y = 100;
+  x = 600;
+  y = 600;
   w = 100;
   h = 100;
-  shapes.push(mk_shape('fill', 'blue', 'rect', [x-w/2,y-h/2,w,h], [
-    mk_shape('stroke', 'blue', 'rect', [x-w/2,y-h/2,w,h]).props
-  ]));
+  shape = mk_shape('rect', 'fill', 'purple', [x-w/2,y-h/2,w,h], [
+    mk_shape('rect', 'stroke', 'purple', [x-w/2,y-h/2,w,h])
+  ]);
+  shapes.push(shape);
 
   link_shapes(shapes.slice(-2));
 
